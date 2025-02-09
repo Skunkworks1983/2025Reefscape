22e2ec56d6b45e4b71904d76fecb630d3edf6ac0
TacoLord346 <TacoLord346@gmail.com>
2025-02-09 14:09:21 -0800

    added new commands and tested on robot

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 import frc.robot.utils.SmartPIDControllerTalonFX;
@@ -102,6 +103,29 @@ public class Collector extends SubsystemBase {
       () -> {
         setCollectorSpeeds(-Constants.Collector.COLLECOR_ROTATE_FAST, 
           Constants.Collector.COLLECOR_ROTATE_FAST);
+        SmartDashboard.putNumber("Right motor current", rightMotor.getSupplyCurrent().getValueAsDouble());
+        SmartDashboard.putNumber("Left motor current", leftMotor.getSupplyCurrent().getValueAsDouble());
+
+      },
+      () -> {
+        setCollectorSpeeds(0, 0);
+      }
+    ).until(
+      () -> {
+        return rightMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF &&
+        leftMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF;
+      }
+    );
+  }
+
+  public Command scorePeiceCommand() {
+    return runEnd(
+      () -> {
+        setCollectorSpeeds(-Constants.Collector.COLLECOR_ROTATE_FAST, 
+          Constants.Collector.COLLECOR_ROTATE_FAST);
+        SmartDashboard.putNumber("Right motor current", rightMotor.getSupplyCurrent().getValueAsDouble());
+        SmartDashboard.putNumber("Left motor current", leftMotor.getSupplyCurrent().getValueAsDouble());
+
       },
       () -> {
         setCollectorSpeeds(0, 0);
@@ -109,4 +133,13 @@ public class Collector extends SubsystemBase {
     );
   }
 
+  public Command waitAfterCatchPeiceCommand() {
+    return Commands.sequence(
+      intakeCoralCommand(),
+      Commands.race(
+        scorePeiceCommand(),
+        Commands.waitSeconds(Constants.Collector.SECONDS_BEFORE_CUTTOF)
+      )
+    );
+  }
 }
