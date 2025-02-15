8161ea3b64af63f57896f8d000573ea13c061cb1
Lucas Hetrick <4031995@g.highlineschools.org>
2025-02-15 15:45:52 -0800

    Update imports

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -8,7 +8,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
-import frc.robot.utils.SmartPIDController;
+import frc.robot.utils.PIDControllers.SmartPIDController;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
