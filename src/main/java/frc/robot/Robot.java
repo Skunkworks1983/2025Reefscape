87c60ea28da6413069aa8a3309b21619b9f0332b
liam <liam.b@skunkworks1983.com>
2025-02-20 20:10:11 -0800

    trying to fix merge problem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,12 +19,11 @@ public class Robot extends TimedRobot {
 
   // replace subsystem with Optional.empty() for testing
   // ENSURE_COMPETITION_READY_SUBSYSTEMS must be false for testing.
+  Optional<Drivebase> drivebase = Optional.of(new Drivebase());
   Optional<Elevator> elevator = Optional.of(new Elevator());
   Optional<Collector> collector = Optional.of(new Collector());
-  Optional<Drivebase> drivebase = Optional.of(new Drivebase());
   Optional<Climber> climber = Optional.of(new Climber());
   
-
   OI oi = new OI( 
     elevator,
     collector,
