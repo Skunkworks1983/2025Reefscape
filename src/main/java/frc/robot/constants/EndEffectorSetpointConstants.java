9222d86fa67d7a4b54926c0cb79c7a455aa84d02
2Carrot10 <eleanorgrutz@gmail.com>
2025-02-21 20:36:51 -0800

    add constants

---
+++ src/main/java/frc/robot/constants/EndEffectorSetpointConstants.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+/** Add your docs here. */
+public class EndEffectorSetpointConstants {
+  public final double elevatorSetpoint;
+  public final double wristSetpoint;
+  public EndEffectorSetpointConstants(double elevatorSetpoint, double wristSetpoint) {
+    this.elevatorSetpoint = elevatorSetpoint;
+    this.wristSetpoint = wristSetpoint;
+
+  }
+}
