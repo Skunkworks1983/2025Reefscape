c4d0913e1475fb35e55af005d939fa895ec7c888
2Carrot10 <eleanorgrutz@gmail.com>
2025-02-25 18:47:50 -0800

    remove import

--- src/main/java/frc/robot/subsystems/drivebase/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drivebase/SwerveModule.java
@@ -16,7 +16,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
