45497a81fe2f143f8d850aa8c91e91bd264ee011
liam <liam.b@skunkworks1983.com>
2025-03-02 12:55:16 -0800

    formating

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -22,15 +22,16 @@ import frc.robot.utils.PIDControllers.SmartPIDControllerTalonFX;
 
 public class Collector extends SubsystemBase {
 
-  private TalonFX rightMotor; 
-  private TalonFX leftMotor; 
+  private TalonFX rightMotor;
+  private TalonFX leftMotor;
 
   private final VelocityVoltage velocityVoltage = new VelocityVoltage(0);
   private double lastRightSpeed;
   private double lastLeftSpeed;
 
   // Neither of these smart PIDs are 'used' after they are constructed because the
-  // PID controller is built into the motor (we don't have to call .calculate like we
+  // PID controller is built into the motor (we don't have to call .calculate like
+  // we
   // do with the PIDController class).
   @SuppressWarnings("unused")
   private SmartPIDControllerTalonFX rightMotorController;
@@ -45,39 +46,36 @@ public class Collector extends SubsystemBase {
 
   /** Creates a new Collector. */
   public Collector() {
-   rightMotor = new TalonFX(Constants.Collector.RIGHT_MOTOR);
-   leftMotor = new TalonFX(Constants.Collector.LEFT_MOTOR);
-   rightMotor.setPosition(0.0);
-   leftMotor.setPosition(0.0);
+    rightMotor = new TalonFX(Constants.Collector.RIGHT_MOTOR);
+    leftMotor = new TalonFX(Constants.Collector.LEFT_MOTOR);
+    rightMotor.setPosition(0.0);
+    leftMotor.setPosition(0.0);
 
-   leftMotor.setInverted(true);
-   setDefaultCommand(holdPositionCommand());
-   
-    
+    leftMotor.setInverted(true);
+    setDefaultCommand(holdPositionCommand());
+
     TalonFXConfiguration talonConfigCollectorMotor = new TalonFXConfiguration();
     talonConfigCollectorMotor.CurrentLimits = CurrentLimits.KRAKEN_CURRENT_LIMIT_CONFIG;
 
-
     talonConfigCollectorMotor.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
-   rightMotor.getConfigurator().apply(talonConfigCollectorMotor);
-   leftMotor.getConfigurator().apply(talonConfigCollectorMotor);
+    rightMotor.getConfigurator().apply(talonConfigCollectorMotor);
+    leftMotor.getConfigurator().apply(talonConfigCollectorMotor);
 
-   rightMotorController = new SmartPIDControllerTalonFX(Constants.Collector.VelocityControlMode.KP,
+    rightMotorController = new SmartPIDControllerTalonFX(Constants.Collector.VelocityControlMode.KP,
         Constants.Collector.VelocityControlMode.KI, Constants.Collector.VelocityControlMode.KD,
         Constants.Collector.VelocityControlMode.KF, Constants.Collector.VelocityControlMode.KV,
         Constants.Collector.VelocityControlMode.KA, Constants.Collector.VelocityControlMode.KS,
-         "right motor",
+        "right motor",
         Constants.Collector.SMART_PID_ENABLED, rightMotor);
-   rightMotorController.AddSlot1Configs(Constants.Collector.PositionControlMode.KP,
-    Constants.Collector.PositionControlMode.KI, Constants.Collector.PositionControlMode.KD,
-    Constants.Collector.PositionControlMode.KF, Constants.Collector.PositionControlMode.KV,
-    Constants.Collector.PositionControlMode.KA, Constants.Collector.PositionControlMode.KS
-  );
+    rightMotorController.AddSlot1Configs(Constants.Collector.PositionControlMode.KP,
+        Constants.Collector.PositionControlMode.KI, Constants.Collector.PositionControlMode.KD,
+        Constants.Collector.PositionControlMode.KF, Constants.Collector.PositionControlMode.KV,
+        Constants.Collector.PositionControlMode.KA, Constants.Collector.PositionControlMode.KS);
 
     leftMotorController = new SmartPIDControllerTalonFX(Constants.Collector.VelocityControlMode.KP,
         Constants.Collector.VelocityControlMode.KI,
-         Constants.Collector.VelocityControlMode.KD,
+        Constants.Collector.VelocityControlMode.KD,
         Constants.Collector.VelocityControlMode.KF, Constants.Collector.VelocityControlMode.KV,
         Constants.Collector.VelocityControlMode.KA, Constants.Collector.VelocityControlMode.KS,
         "left motor",
@@ -85,17 +83,17 @@ public class Collector extends SubsystemBase {
     leftMotorController.AddSlot1Configs(Constants.Collector.PositionControlMode.KP,
         Constants.Collector.PositionControlMode.KI, Constants.Collector.PositionControlMode.KD,
         Constants.Collector.PositionControlMode.KF, Constants.Collector.PositionControlMode.KV,
-        Constants.Collector.PositionControlMode.KA, Constants.Collector.PositionControlMode.KS
-      );
-    
-        beambreak = new DigitalInput(Constants.Collector.DIGITAL_INPUT_CHANNEL);
+        Constants.Collector.PositionControlMode.KA, Constants.Collector.PositionControlMode.KS);
+
+    beambreak = new DigitalInput(Constants.Collector.DIGITAL_INPUT_CHANNEL);
   }
 
-  // meters per sec 
-  private void setCollectorSpeeds(double rightSpeed, double leftSpeed){
+  // meters per sec
+  private void setCollectorSpeeds(double rightSpeed, double leftSpeed) {
     if (rightSpeed != lastRightSpeed) {
       rightMotor.setControl(velocityVoltage
-          .withVelocity(rightSpeed * Constants.Collector.COLLECTOR_ROTATIONS_PER_METER).withEnableFOC(true).withSlot(0));
+          .withVelocity(rightSpeed * Constants.Collector.COLLECTOR_ROTATIONS_PER_METER).withEnableFOC(true)
+          .withSlot(0));
       ConditionalSmartDashboard.putNumber("Collector/Right speed", rightSpeed);
     }
     lastRightSpeed = rightSpeed;
@@ -107,16 +105,19 @@ public class Collector extends SubsystemBase {
     }
     lastLeftSpeed = leftSpeed;
   }
+
   @Override
   public void periodic() {
     leftMotorController.updatePID();
     rightMotorController.updatePID();
-    
-    ConditionalSmartDashboard.putNumber("Collector/Right motor current", rightMotor.getSupplyCurrent().getValueAsDouble());
-    ConditionalSmartDashboard.putNumber("Collector/Left motor current", leftMotor.getSupplyCurrent().getValueAsDouble());
+
+    ConditionalSmartDashboard.putNumber("Collector/Right motor current",
+        rightMotor.getSupplyCurrent().getValueAsDouble());
+    ConditionalSmartDashboard.putNumber("Collector/Left motor current",
+        leftMotor.getSupplyCurrent().getValueAsDouble());
     ConditionalSmartDashboard.putBoolean("Collector/Beambreak collector", !beambreak.get());
   }
-  
+
   public double getLeftMotorVelocity() {
     return leftMotor.getVelocity().getValueAsDouble();
   }
@@ -125,11 +126,11 @@ public class Collector extends SubsystemBase {
     return rightMotor.getVelocity().getValueAsDouble();
   }
 
-  public double getRightMotorPosition(){
+  public double getRightMotorPosition() {
     return rightMotor.getPosition().getValueAsDouble();
   }
 
-  public double getLeftMotorPosition(){
+  public double getLeftMotorPosition() {
     return leftMotor.getPosition().getValueAsDouble();
   }
 
@@ -142,161 +143,140 @@ public class Collector extends SubsystemBase {
 
   public Command rotateCoralCommand() {
     return runEnd(
-      () -> {
-        setCollectorSpeeds(Constants.Collector.CORAL_INTAKE_SLOW_SPEED, 
-        Constants.Collector.CORAL_INTAKE_FAST_SPEED);
-        ConditionalSmartDashboard.putNumber("Collector/right collector current speed", getRightMotorVelocity());
-        ConditionalSmartDashboard.putNumber("Collector/left collector current speed", getLeftMotorVelocity());
-      }, 
-      () -> {
-        setCollectorSpeeds(0, 0);
-      }
-    );
+        () -> {
+          setCollectorSpeeds(Constants.Collector.CORAL_INTAKE_SLOW_SPEED,
+              Constants.Collector.CORAL_INTAKE_FAST_SPEED);
+          ConditionalSmartDashboard.putNumber("Collector/right collector current speed", getRightMotorVelocity());
+          ConditionalSmartDashboard.putNumber("Collector/left collector current speed", getLeftMotorVelocity());
+        },
+        () -> {
+          setCollectorSpeeds(0, 0);
+        });
   }
 
-  int endCount [] = {0}; // This value needs to be effectivly final 
+  int endCount[] = { 0 }; // This value needs to be effectivly final
 
   // Rrue if you want it to stop the motor when the command ends
-  // it should almost always be true unless there will be a following command right after that will end it
+  // it should almost always be true unless there will be a following command
+  // right after that will end it
   public Command intakeCoralCommand(
-    boolean stopOnEnd
-  ) {
+      boolean stopOnEnd) {
     return runEnd(
-      () -> {
-        setCollectorSpeeds(Constants.Collector.CORAL_INTAKE_FAST_SPEED, 
-          Constants.Collector.CORAL_INTAKE_FAST_SPEED * Constants.Collector.SPEED_MULIPILER_LEFT);
-      },
-      () -> {
-        if(stopOnEnd) {
-          setCollectorSpeeds(0, 0);
-        }
-      }
-    ).until(
-      () -> {
-        ConditionalSmartDashboard.putNumber("Collector/Amp cut off right", rightMotor.getSupplyCurrent().getValueAsDouble());
-        ConditionalSmartDashboard.putNumber("Collector/Amp cut off left", leftMotor.getSupplyCurrent().getValueAsDouble());
-        if (rightMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF ||
-            leftMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF ||
-            rightMotor.getSupplyCurrent().getValueAsDouble() < 0 ||
-            leftMotor.getSupplyCurrent().getValueAsDouble() < 0) 
-        {
-          endCount[0]++;
-        }
-        else
-        {
-          endCount[0] = 0;
-        }
-        return endCount[0] >= Constants.Collector.END_COUNT_TICK_COUNTER;
-      }
-    );
+        () -> {
+          setCollectorSpeeds(Constants.Collector.CORAL_INTAKE_FAST_SPEED,
+              Constants.Collector.CORAL_INTAKE_FAST_SPEED * Constants.Collector.SPEED_MULIPILER_LEFT);
+        },
+        () -> {
+          if (stopOnEnd) {
+            setCollectorSpeeds(0, 0);
+          }
+        }).until(
+            () -> {
+              ConditionalSmartDashboard.putNumber("Collector/Amp cut off right",
+                  rightMotor.getSupplyCurrent().getValueAsDouble());
+              ConditionalSmartDashboard.putNumber("Collector/Amp cut off left",
+                  leftMotor.getSupplyCurrent().getValueAsDouble());
+              if (rightMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF
+                  ||
+                  leftMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF ||
+                  rightMotor.getSupplyCurrent().getValueAsDouble() < 0 ||
+                  leftMotor.getSupplyCurrent().getValueAsDouble() < 0) {
+                endCount[0]++;
+              } else {
+                endCount[0] = 0;
+              }
+              return endCount[0] >= Constants.Collector.END_COUNT_TICK_COUNTER;
+            });
   }
 
   public Command scoreCoralCommand() {
     return runEnd(
-      () -> {
-        setCollectorSpeeds(Constants.Collector.CORAL_INTAKE_FAST_SPEED, 
-          Constants.Collector.CORAL_INTAKE_FAST_SPEED);
-      },
-      () -> {
-        setCollectorSpeeds(0, 0);
-      }
-    ).until(
-      () -> {
-        if (rightMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF &&
-        leftMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF) 
-        {
-          endCount[0]++;
-        }
-        else
-        {
-          endCount[0] = 0;
-        }
-        return endCount[0] > Constants.Collector.END_COUNT_TICK_COUNTER;
-      }
-    );
+        () -> {
+          setCollectorSpeeds(Constants.Collector.CORAL_INTAKE_FAST_SPEED,
+              Constants.Collector.CORAL_INTAKE_FAST_SPEED);
+        },
+        () -> {
+          setCollectorSpeeds(0, 0);
+        }).until(
+            () -> {
+              if (rightMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF
+                  &&
+                  leftMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.COLLECTOR_AMPS_BEFORE_CUTTOF) {
+                endCount[0]++;
+              } else {
+                endCount[0] = 0;
+              }
+              return endCount[0] > Constants.Collector.END_COUNT_TICK_COUNTER;
+            });
   }
 
   public Command waitAfterCatchPieceCommand() {
     return Commands.sequence(
-      intakeCoralCommand(false),
-      Commands.race(
-        scoreCoralCommand(),
-        Commands.waitSeconds(Constants.Collector.SECONDS_BEFORE_CUTTOF)
-      )
-    );
+        intakeCoralCommand(false),
+        Commands.race(
+            scoreCoralCommand(),
+            Commands.waitSeconds(Constants.Collector.SECONDS_BEFORE_CUTTOF)));
   }
 
-  public Command expelCoral(boolean stopOnEnd)
-  {
+  public Command expelCoral(boolean stopOnEnd) {
     return runEnd(
-      () -> {
-        setCollectorSpeeds(Constants.Collector.COLLECTOR_REVERSE, 
-          Constants.Collector.COLLECTOR_REVERSE);
-      },
-      () -> {
-        if(stopOnEnd) {
-          setCollectorSpeeds(0, 0);
-        }
-      }
-    );
+        () -> {
+          setCollectorSpeeds(Constants.Collector.COLLECTOR_REVERSE,
+              Constants.Collector.COLLECTOR_REVERSE);
+        },
+        () -> {
+          if (stopOnEnd) {
+            setCollectorSpeeds(0, 0);
+          }
+        });
 
   }
 
-  public Command holdPositionCommand()
-  {
+  public Command holdPositionCommand() {
     return startEnd(
-      () -> {
-        setCollectorSetPoint(getRightMotorPosition(), getLeftMotorPosition());
-      },
-      () -> {
+        () -> {
+          setCollectorSetPoint(getRightMotorPosition(), getLeftMotorPosition());
+        },
+        () -> {
 
-      }
-    );
+        });
   }
 
   public Command intakeAlgaeCommand(
-    boolean stopOnEnd
-  ) {
+      boolean stopOnEnd) {
     return runEnd(
-      () -> {
-        setCollectorSpeeds(Constants.Collector.ALGAE_INTAKE, 
-          Constants.Collector.ALGAE_INTAKE);
-      },
-      () -> {
-        if(stopOnEnd) {
-          setCollectorSpeeds(0, 0);
-        }
-      }
-    ).until(
-      () -> {
-        if (rightMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.ALGAE_AMP_CUT_OFF &&
-        leftMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.ALGAE_AMP_CUT_OFF) 
-        {
-          endCount[0]++;
-        }
-        else
-        {
-          endCount[0] = 0;
-        }
-        return endCount[0] > Constants.Collector.END_COUNT_TICK_COUNTER;
-      }
-    );
+        () -> {
+          setCollectorSpeeds(Constants.Collector.ALGAE_INTAKE,
+              Constants.Collector.ALGAE_INTAKE);
+        },
+        () -> {
+          if (stopOnEnd) {
+            setCollectorSpeeds(0, 0);
+          }
+        }).until(
+            () -> {
+              if (rightMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.ALGAE_AMP_CUT_OFF &&
+                  leftMotor.getSupplyCurrent().getValueAsDouble() >= Constants.Collector.ALGAE_AMP_CUT_OFF) {
+                endCount[0]++;
+              } else {
+                endCount[0] = 0;
+              }
+              return endCount[0] > Constants.Collector.END_COUNT_TICK_COUNTER;
+            });
   }
 
   public Command expelAlgaeCommand(
-    boolean stopOnEnd
-  ) {
+      boolean stopOnEnd) {
     return runEnd(
-      () -> {
-        setCollectorSpeeds(Constants.Collector.ALGAE_EXPEL, 
-          Constants.Collector.ALGAE_EXPEL);
-      },
-      () -> {
-        if(stopOnEnd) {
-          setCollectorSpeeds(0, 0);
-        }
-      }
-    );
+        () -> {
+          setCollectorSpeeds(Constants.Collector.ALGAE_EXPEL,
+              Constants.Collector.ALGAE_EXPEL);
+        },
+        () -> {
+          if (stopOnEnd) {
+            setCollectorSpeeds(0, 0);
+          }
+        });
   }
-  
+
 }
--- src/main/java/frc/robot/subsystems/OI.java
+++ src/main/java/frc/robot/subsystems/OI.java
@@ -22,7 +22,6 @@ public class OI {
   private Joystick rotationJoystick = new Joystick(Joysticks.ROTATION_JOYSTICK_ID);
   private Joystick translationJoystick = new Joystick(Joysticks.TRANSLATION_JOYSTICK_ID);
   private Joystick buttonJoystick = new Joystick(Joysticks.BUTTON_STICK_ID);
-  
 
   // Input to the function could be x or y axis.
   private DoubleFunction<Double> joystickToMetersPerSecond = (
@@ -41,52 +40,52 @@ public class OI {
       : axisInput;
 
   public OI(
-    Optional<Elevator> optionalElevator, 
-    Optional<Collector> optionalCollector,
-    Optional<Wrist> optionalWrist,
-    Optional<Climber> optionalClimber,
-    Optional<Drivebase> optionalDrivebase,
-    Optional<Funnel> optionalFunnel) {
-      Trigger algaeToggle = new JoystickButton(buttonJoystick, Constants.OI.IDs.Buttons.ALGAE_TOGGLE);
-      Trigger coralToggle = algaeToggle.negate(); // If not in algae mode, we are using coral mode.
+      Optional<Elevator> optionalElevator,
+      Optional<Collector> optionalCollector,
+      Optional<Wrist> optionalWrist,
+      Optional<Climber> optionalClimber,
+      Optional<Drivebase> optionalDrivebase,
+      Optional<Funnel> optionalFunnel) {
+    Trigger algaeToggle = new JoystickButton(buttonJoystick, Constants.OI.IDs.Buttons.ALGAE_TOGGLE);
+    Trigger coralToggle = algaeToggle.negate(); // If not in algae mode, we are using coral mode.
 
     if (optionalCollector.isPresent()) {
       Collector collector = optionalCollector.get();
 
       new JoystickButton(buttonJoystick, Constants.OI.IDs.Buttons.INTAKE)
-        .and(coralToggle)
-        .whileTrue(collector.intakeCoralCommand(true));
+          .and(coralToggle)
+          .whileTrue(collector.intakeCoralCommand(true));
 
       new JoystickButton(buttonJoystick, Constants.OI.IDs.Buttons.EXPEL)
-        .and(coralToggle)
-        .whileTrue(collector.expelCoral(true));
+          .and(coralToggle)
+          .whileTrue(collector.expelCoral(true));
 
       new JoystickButton(buttonJoystick, Constants.OI.IDs.Buttons.INTAKE)
-        .and(algaeToggle)
-        .whileTrue(collector.intakeAlgaeCommand(true));
+          .and(algaeToggle)
+          .whileTrue(collector.intakeAlgaeCommand(true));
 
       new JoystickButton(buttonJoystick, Constants.OI.IDs.Buttons.EXPEL)
-        .and(algaeToggle)
-        .whileTrue(collector.expelAlgaeCommand(true));
+          .and(algaeToggle)
+          .whileTrue(collector.expelAlgaeCommand(true));
     }
 
-    if(optionalClimber.isPresent()) {
+    if (optionalClimber.isPresent()) {
       Climber climber = optionalClimber.get();
       new JoystickButton(buttonJoystick, Constants.OI.IDs.Buttons.CLIMBER_GOTO_MAX)
-        .onTrue(climber.goToPositionAfterMagnetSensor(Constants.Climber.CLIMBER_MAX));
+          .onTrue(climber.goToPositionAfterMagnetSensor(Constants.Climber.CLIMBER_MAX));
       new JoystickButton(buttonJoystick, Constants.OI.IDs.Buttons.CLIMBER_GOTO_MIN)
-        .onTrue(climber.goToPositionAfterMagnetSensor(Constants.Climber.CLIMBER_MIN));
+          .onTrue(climber.goToPositionAfterMagnetSensor(Constants.Climber.CLIMBER_MIN));
     }
 
-    if(optionalFunnel.isPresent()){
+    if (optionalFunnel.isPresent()) {
       Funnel funnel = optionalFunnel.get();
       new JoystickButton(translationJoystick, Constants.OI.IDs.Buttons.FUNNEL_GO_TO_MAX)
-        .onTrue(new MoveFunnelToSetpoint(funnel, Constants.Funnel.FUNNEL_POSITION_HIGH_CONVERTED));
+          .onTrue(new MoveFunnelToSetpoint(funnel, Constants.Funnel.FUNNEL_POSITION_HIGH_CONVERTED));
       new JoystickButton(translationJoystick, Constants.OI.IDs.Buttons.FUNNEL_GO_TO_MIN)
-        .onTrue(new MoveFunnelToSetpoint(funnel, Constants.Funnel.FUNNEL_POSITION_LOW_CONVERTED));
+          .onTrue(new MoveFunnelToSetpoint(funnel, Constants.Funnel.FUNNEL_POSITION_LOW_CONVERTED));
     }
 
-    if(optionalElevator.isPresent() && optionalWrist.isPresent()) {
+    if (optionalElevator.isPresent() && optionalWrist.isPresent()) {
       Elevator elevator = optionalElevator.get();
       Wrist wrist = optionalWrist.get();
 
@@ -99,53 +98,41 @@ public class OI {
 
       // Algae mode
       endEffectorGround.and(algaeToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeGround)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeGround));
 
       endEffectorStow.and(algaeToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeStow)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeStow));
 
       endEffectorToScoreLow.and(algaeToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeProcessor)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeProcessor));
 
       endEffectorToL2.and(algaeToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeL2)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeL2));
 
       endEffectorToL3.and(algaeToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeL3)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeL3));
 
       endEffectorToScoreHigh.and(algaeToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeNet)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.algaeNet));
 
       // Coral mode
       endEffectorGround.and(coralToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralGround)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralGround));
 
       endEffectorStow.and(coralToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralStow)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralStow));
 
       endEffectorToScoreLow.and(coralToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralL1)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralL1));
 
       endEffectorToL2.and(coralToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralL2)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralL2));
 
       endEffectorToL3.and(coralToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralL3)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralL3));
 
       endEffectorToScoreHigh.and(coralToggle).onTrue(
-        new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralL4)
-      );
+          new MoveEndEffector(elevator, wrist, EndEffectorSetpoints.coralL4));
     }
   }
 
