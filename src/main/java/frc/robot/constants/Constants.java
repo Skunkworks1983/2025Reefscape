d419853cfeee7f5b955a09f251ed7b17cf0dac54
2Carrot10 <eleanorgrutz@gmail.com>
2025-03-03 20:04:58 -0800

    Add wrist DIO IDs.

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -256,8 +256,8 @@ public class Constants {
     // For determining right and left, look at the elevator from the side paralel to the one that the elevator is on
     public static final int MOTOR_RIGHT_ID = 7; // Temp id
     public static final int MOTOR_LEFT_ID = 7; // Temp id
-    public static final int BOTTOM_LIMIT_SWITCH_ID = 4; // Temp id
-    public static final int TOP_LIMIT_SWITCH_ID = 5; // Temp id
+    public static final int BOTTOM_LIMIT_SWITCH_ID = 5; // Temp id
+    public static final int TOP_LIMIT_SWITCH_ID = 6; // Temp id
 
     // This tolerance value will be used for deciding if the elevator
     // should target to its setpoint or if the setpoint is too far
