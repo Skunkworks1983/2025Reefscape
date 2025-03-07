ab98c079bea24f9aa1a09d9749775802e6489b0c
TacoLord346 <TacoLord346@gmail.com>
2025-03-06 21:33:52 -0800

    temp commit

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -323,7 +323,7 @@ public class Constants {
     public static final double WRIST_STOW_POSITION_ALGAE = 0.1;
 
     public static final EndEffectorSetpointConstants ALGAE_GROUND = 
-      new EndEffectorSetpointConstants(0.0, 0.0, WRIST_STOW_POSITION_ALGAE);
+      new EndEffectorSetpointConstants(0.0, 0.3941, WRIST_STOW_POSITION_ALGAE);
     public static final EndEffectorSetpointConstants ALGAE_STOW = 
       new EndEffectorSetpointConstants(0.0, WRIST_STOW_POSITION_ALGAE, WRIST_STOW_POSITION_ALGAE);
     public static final EndEffectorSetpointConstants ALGAE_PROCESSOR = 
@@ -342,7 +342,7 @@ public class Constants {
     public static final EndEffectorSetpointConstants CORAL_L1 = 
       new EndEffectorSetpointConstants(0.0, 0.0, WRIST_STOW_POSITION_CORAL);
     public static final EndEffectorSetpointConstants CORAL_L2 = 
-      new EndEffectorSetpointConstants(13.756, 0.0, WRIST_STOW_POSITION_CORAL);
+      new EndEffectorSetpointConstants(13.756, 0.1441, WRIST_STOW_POSITION_CORAL);
     public static final EndEffectorSetpointConstants CORAL_L3 = 
       new EndEffectorSetpointConstants(24.914, 0.0, WRIST_STOW_POSITION_CORAL);
     public static final EndEffectorSetpointConstants CORAL_L4 = 
