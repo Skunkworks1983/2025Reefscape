0a7073d9ec553444e5cb967bd03ab82585a831d2
Lucas Hetrick <4031995@g.highlineschools.org>
2025-03-08 12:54:46 -0800

    add motion profile base drive out

---
+++ src/main/java/frc/robot/commands/drivebase/TrapezoidProfileDriveOut.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.drivebase;
+
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drivebase.Drivebase;
+
+public class TrapezoidProfileDriveOut extends Command {
+
+  Drivebase drivebase;
+  Timer timeElasped = new Timer();
+  TrapezoidProfile profile = new TrapezoidProfile(new Constraints(1.0, 5.0));
+  State startState = new State();
+  State goalState = new State(1.0, 0.0);
+
+  public TrapezoidProfileDriveOut(Drivebase drivebase) {
+    this.drivebase = drivebase;
+    addRequirements(drivebase);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    timeElasped.reset();
+    timeElasped.start();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double xVelocity = profile.calculate(timeElasped.get(), startState, goalState).velocity;
+    drivebase.drive(xVelocity, 0.0, 0.0, false);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return timeElasped.get() > 2.0;
+  }
+}
